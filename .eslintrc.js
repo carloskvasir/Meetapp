c18651e315bcb29de7934bd73a1919f8c9d649b3
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-11-18 22:10:47 -0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     es6: true,
     node: true,
+    jest: true,
   },
   extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
@@ -15,14 +16,14 @@ module.exports = {
   },
   rules: {
     //prettier enviar errors
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
     // todos os metodos da clase usam this
-    "class-methods-use-this": 'off',
+    'class-methods-use-this': 'off',
     // não pode manipular parametros recebidos
-    "no-param-reassign": 'off',
+    'no-param-reassign': 'off',
     // use apenas camelCase
-    "camelcase": 'off',
+    'camelcase': 'off',
     // Não de erro quando não utilizar a var next do express
-    "no-unused-vars": ['error', { "argsIgnorePattern": "next" }],
+    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
   },
 };
