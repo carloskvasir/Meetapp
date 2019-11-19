b5d4da6171932077b9fe6e370dbc40db74527924
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-11-18 23:53:07 -0300

    hash password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,17 +1,27 @@
 import Sequelize, { Model } from 'sequelize';
 
+import bcrypt from 'bcryptjs';
+
 class User extends Model {
   static init(connection) {
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize: connection,
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password)
+        user.password_hash = await bcrypt.hash(user.password, 8);
+    });
+
+    return this;
   }
 }
 
