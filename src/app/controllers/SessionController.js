fb1c4f5b7af889a7dbeb8fb00c6af022ac72b323
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-11-19 11:44:45 -0300

    implements Session store

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    // This user exists?
+    if (!user) return res.status(401).json({ error: 'User not found 😒' });
+
+    // This is right pass?
+    if (!(await user.checkThisPassword(password)))
+      return res.status(401).json({ error: 'Pass does not match' });
+
+    const { id, name } = user;
+
+    return res.json({
+      user: { id, name, email },
+
+      // Data for jwt
+      token: jwt.sign({ id }, authConfig.secretKey, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
