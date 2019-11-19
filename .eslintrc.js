9a4ac815b01e207e5a21460f9fe490445276ac9c
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-11-18 21:58:46 -0300

    fix error with "

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
+  env: {
+    es6: true,
+    node: true,
   },
-  "extends": ['airbnb-base', 'prettier'],
-  "plugins": ['prettier'],
-
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "rules": {
+  rules: {
     //prettier enviar errors
     "prettier/prettier": "error",
     // todos os metodos da clase usam this
-    "class-methods-use-this": "off",
+    "class-methods-use-this": 'off',
     // não pode manipular parametros recebidos
-    "no-param-ressign": "off",
+    "no-param-reassign": 'off',
     // use apenas camelCase
-    "camelcase": "off",
+    "camelcase": 'off',
     // Não de erro quando não utilizar a var next do express
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
-  }
+    "no-unused-vars": ['error', { "argsIgnorePattern": "next" }],
+  },
 };
